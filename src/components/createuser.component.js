c1f5b505825e2c0537ec2cff6f7380faf4467678
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-23 16:52:55 +0530

    create user component complete

--- src/components/createuser.component.js
+++ src/components/createuser.component.js
@@ -1,8 +1,55 @@
 import React from "react";
+import axios from "axios";
 
 class CreateUsr extends React.Component {
+  state = {
+    username: "",
+    showLog: false,
+  };
+  onsubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      username: this.state.username,
+    };
+    console.log(user);
+    this.setState({ username: "", showLog: true });
+    setTimeout(() => {
+      this.setState({ showLog: false });
+    }, 2000);
+
+    axios
+      .post("http://localhost:5000/users/add", user)
+      .then((res) => console.log(res.data));
+  };
   render() {
-    return <div>heeloooooo</div>;
+    return (
+      <div>
+        <h3>Create new user</h3>
+        <form onSubmit={this.onsubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+          {this.state.showLog ? (
+            <div>
+              <h3> {this.state.username} New user added</h3>
+            </div>
+          ) : null}
+        </form>
+      </div>
+    );
   }
 }
 
